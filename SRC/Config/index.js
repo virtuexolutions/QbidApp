03991c096899f6e9624d77346f7c71d74170149a
meerab11 <meerab.gill@virtuexolutions.com>
2023-11-17 13:14:01 +0500

    changes by meerab

--- SRC/Config/index.js
+++ SRC/Config/index.js
@@ -1,4 +1,4 @@
-export const baseUrl = "https://530f-139-190-235-42.ngrok-free.app";
+export const baseUrl = "https://cf0e-139-190-235-42.ngrok-free.app";
 // export const baseUrl = "https://4b54-113-203-241-1.in.ngrok.io";
 export const imageUrl = `${baseUrl}/api/images/`;
 export const profilePicUrl = `${baseUrl}/uploads`
